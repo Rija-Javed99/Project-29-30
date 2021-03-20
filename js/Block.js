fedcdf5f7a4c00e5e430ab6d6fde342663a7671a
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-03-20 16:18:03 +0530

    Update Block.js

--- js/Block.js
+++ js/Block.js
@@ -7,7 +7,7 @@ class Block{
             //isStatic:true
            
         }
-        this.visibility = 225;
+       // this.visibility
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
@@ -16,8 +16,8 @@ class Block{
       }
      
       display(){
-        console.log(this.body.speed);
-        if(this.body.speed <3){
+       
+        //this.body.speed <3
         var angle = this.body.angle;
         var pos= this.body.position;
         push();
@@ -27,10 +27,8 @@ class Block{
         rect(0,0,this.width, this.height);
         pop();
       }else{
-        World.remove(world, this.body);
-        push();
-        this.visibility = this.visibility -5;
-        pop();
+        //World.remove
+       
         
       }
     }
